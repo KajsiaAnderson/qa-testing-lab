85ec1b4700ad3fe95899bd683edbf5d72d84ea2e
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-10-31 16:59:27 -0600

    added Describe function and checked data type

--- functions.js
+++ functions.js
@@ -7,6 +7,15 @@ module.exports = {
     },
     add: (num1, num2) => {
         return num1 + num2
+    },
+    multiply: (num1, num2) => {
+        return num1 * num2
+    },
+    divide: (num1, num2) => {
+        return num1 / num2
+    },
+    subtract: (num1, num2) => {
+        return num1 - num2
     }
 }
 
--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,13 @@
 const {
-    returnTwo, 
-    greeting, 
-    add
+    returnTwo,
+    greeting,
+    add,
 } = require('./functions.js')
+
+//another way to require function.js (using for multiply, divide, and subtract functions)
+const challenge = require('./functions')
+
+
 
 test('test returnTwo', () => {
     expect(returnTwo()).toEqual(2)
@@ -13,7 +18,32 @@ test('test greeting', () => {
     expect(greeting('Jill')).toEqual("Hello, Jill.")
 })
 
-test('test add', () => {
-    expect(add(1, 2)).toEqual(3)
-    expect(add(5, 9)).toEqual(14)
+
+//use Describe
+describe('Math functions', () => {
+    test('test add', () => {
+        expect(add(1, 2)).toEqual(3)
+        expect(add(5, 9)).toEqual(14)
+    })
+
+    test('test multiply', () => {
+        expect(challenge.multiply(3, 4)).toEqual(12)
+        expect(challenge.multiply(6, 10)).toEqual(60)
+    })
+
+    test('test divide', () => {
+        expect(challenge.divide(9, 3)).toEqual(3)
+        expect(challenge.divide(6, 3)).toEqual(2)
+    })
+
+    test('test subtract', () => {
+        expect(challenge.subtract(3, 4)).toEqual(-1)
+        expect(challenge.subtract(10, 6)).toEqual(4)
+    })
+})
+
+
+//check data types
+test('test data type of subtract', () => {
+    expect(typeof challenge.subtract()).toBe('number')
 })
